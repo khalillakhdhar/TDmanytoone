7a4e4b5022df8e5c5f89e6cfe1fb57640d2422ff
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 14:41:45 +0100

    Update Comment.java

--- src/main/java/com/http/tp/entities/Comment.java
+++ src/main/java/com/http/tp/entities/Comment.java
@@ -1,8 +1,25 @@
 package com.http.tp.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import com.sun.istack.NotNull;
 
 @Entity
 public class Comment extends AuditModel {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
+@Lob
+@NotNull
+private String content;
+
+
+
+
+
 
 }
