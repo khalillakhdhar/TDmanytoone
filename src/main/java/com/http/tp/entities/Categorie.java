b0c2ed5dc5f839d79b789bdea6cfb5c7a3937e6f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 15:50:36 +0100

    Create Categorie.java

---
+++ src/main/java/com/http/tp/entities/Categorie.java
@@ -0,0 +1,33 @@
+package com.http.tp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categorie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(unique = true)
+	private String titre;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	
+	
+	
+}
